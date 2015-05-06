14b8fc2508cf0c40407ef6fe75985ef1e6ca4aac
filippov <filippov70@gmail.com>
2015-05-06 11:20:21 +0700

    файлы для земкома

--- js/app.js
+++ js/app.js
@@ -20,67 +20,113 @@
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //SOFTWARE.
 
+var convertedGeometry = null;
+var zemcom = '';
+
+function parseOLPolygon(polygon) {
+    for (var i = 0; i < polygon.getLinearRingCount(); i++) {
+        if (i > 0) {
+            zemcom += '\n';
+            console.log('contour');
+        }
+        var ring = polygon.getLinearRing(i);
+        var coords = ring.getCoordinates();
+        for (var k = 0; k < coords.length; k++) {
+            var point = coords[k];
+            var x = point[0];
+            var y = point[1];
+            //console.log(point);
+            zemcom += x + ' ' + y + '\n';
+            console.log('x = ' + x + ' y = ' + y);
+        }
+    }
+}
 
 function convert() {
-    $("#data");
+    //$("#data");
 //    wkt = 'POLYGON((10.689697265625 -25.0927734375, 34.595947265625 ' +
 //    '-20.1708984375, 38.814697265625 -35.6396484375, 13.502197265625 ' +
 //    '-39.1552734375, 10.689697265625 -25.0927734375))';
     var inputdata = $("#data").val();
-    var contours = inputdata.split('\n\n');
-    var wkt = '';
-    var geometryType = $("#gt").val();
-    var contoursCoords = [];
+    // Проверка на содержимое
+    // Допустимо два варианта: а) из гиспортала, б) из WKT QGIS 
 
-    for (var i = 0; i < contours.length; i++) {
-        var cntwkt = '(';
-        var contour = contours[i];
-        var cntdata = contour.split('\n');
-        for (var j = 0; j < cntdata.length; j++) {
-            var coords = cntdata[j].split(' ');
-            var x = parseFloat(coords[0].replace(',', '.'));
-            var y = parseFloat(coords[1].replace(',', '.'));
-            var wktc = x + ' ' + y;
-            if (j !== cntdata.length - 1) {
-                wktc += ', ';
-            }
-            cntwkt += wktc;
-            //console.log(replace);
+    if ((inputdata.indexOf('POLYGON') === 0) || (inputdata.indexOf('MULTIPOLYGON') === 0)) {
+        // Перевод WKT в CSV-земкома
+        mapit(inputdata);
+        //$("#data").val(convertedGeometry);
+        console.log(convertedGeometry);
+        if (convertedGeometry instanceof ol.geom.Polygon) {
+            console.log('polygon');
+            parseOLPolygon(convertedGeometry);
         }
-        cntwkt += ')';
-        contoursCoords.push(cntwkt);
-    }
-
-    if (geometryType === 'POLYGON') {
-        wkt = 'POLYGON(';
-        for (var i = 0; i < contoursCoords.length; i++) {
-            wkt += contoursCoords[i];
-            if (i !== contoursCoords.length - 1) {
-                wkt += ',';
+        else if (convertedGeometry instanceof ol.geom.MultiPolygon) {
+            console.log('multipolygon');
+            var contours = convertedGeometry.getPolygons();
+            for (var i = 0; i < contours.length; i++) {
+                if (i > 0) {
+                    zemcom += '\n';
+                    console.log('Mcontour');
+                }
+                parseOLPolygon(contours[i]);
             }
         }
-        wkt += ')';
-    }
-    else {
-        wkt = 'MULTIPOLYGON((';
-        for (var i = 0; i < contoursCoords.length; i++) {
-            wkt += contoursCoords[i];
-            if (i !== contoursCoords.length - 1) {
-                wkt += '), (';
+        $("#data").val(zemcom);
+    } else {
+        var contours = inputdata.split('\n\n');
+        var wkt = '';
+        var geometryType = $("#gt").val();
+        var contoursCoords = [];
+
+        for (var i = 0; i < contours.length; i++) {
+            var cntwkt = '(';
+            var contour = contours[i];
+            var cntdata = contour.split('\n');
+            for (var j = 0; j < cntdata.length; j++) {
+                var coords = cntdata[j].split(' ');
+                var x = parseFloat(coords[0].replace(',', '.'));
+                var y = parseFloat(coords[1].replace(',', '.'));
+                var wktc = x + ' ' + y;
+                if (j !== cntdata.length - 1) {
+                    wktc += ', ';
+                }
+                cntwkt += wktc;
+                //console.log(replace);
             }
+            cntwkt += ')';
+            contoursCoords.push(cntwkt);
         }
-        wkt += '))';
+
+        if (geometryType === 'POLYGON') {
+            wkt = 'POLYGON(';
+            for (var i = 0; i < contoursCoords.length; i++) {
+                wkt += contoursCoords[i];
+                if (i !== contoursCoords.length - 1) {
+                    wkt += ',';
+                }
+            }
+            wkt += ')';
+        }
+        else {
+            wkt = 'MULTIPOLYGON((';
+            for (var i = 0; i < contoursCoords.length; i++) {
+                wkt += contoursCoords[i];
+                if (i !== contoursCoords.length - 1) {
+                    wkt += '), (';
+                }
+            }
+            wkt += '))';
+        }
+        $("#data").val(wkt);
+        mapit(wkt);
     }
-
-    $("#data").val(wkt);
-
-    mapit(wkt);
 }
 
 function mapit(wkt) {
-    console.log(wkt);
+    //console.log(wkt);
     var format = new ol.format.WKT();
     var feature = format.readFeature(wkt);
+    convertedGeometry = feature.getGeometry();
     //feature.getGeometry().transform('EPSG:4326', 'EPSG:3857');
 
     var styles = [
@@ -113,7 +159,7 @@ function mapit(wkt) {
 //                var coordinates = feature.getGeometry().getCoordinates()[0];
 //                return new ol.geom.MultiPoint(coordinates);
 //            }
- //       })
+                //       })
     ];
 
     var vector = new ol.layer.Vector({
